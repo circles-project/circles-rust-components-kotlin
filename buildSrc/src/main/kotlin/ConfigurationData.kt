9053be3749e74c3e4392adb464d7a5faa8d1637c
Taras Smakula <tarassmakula@gmail.com>
2023-11-08 18:19:42 +0200

    Configure publish

--- buildSrc/src/main/kotlin/ConfigurationData.kt
+++
@@ -1,7 +0,0 @@
-object ConfigurationData {
-    const val compileSdk = 33
-    const val targetSdk = 33
-    const val minSdk = 21
-    const val publishGroupId = "org.circles.rustcomponents"
-    const val versionNameCrypto = "0.3.15.1"
-}
