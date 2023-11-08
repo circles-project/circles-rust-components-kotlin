c61539ea6cfd359a34dcefd04d28cf44eff202c2
Taras Smakula <tarassmakula@gmail.com>
2023-11-08 17:09:17 +0200

    Remove

--- buildSrc/src/main/kotlin/BuildVersionsSDK.kt
+++
@@ -1,5 +0,0 @@
-object BuildVersionsSDK {
-	const val majorVersion = 0
-	const val minorVersion = 1
-	const val patchVersion = 67
-}
--- buildSrc/src/main/kotlin/ConfigurationData.kt
+++ buildSrc/src/main/kotlin/ConfigurationData.kt
@@ -2,9 +2,6 @@ object ConfigurationData {
     const val compileSdk = 33
     const val targetSdk = 33
     const val minSdk = 21
-    const val publishGroupId = "org.matrix.rustcomponents"
-    const val versionNameCrypto =
-        "${BuildVersionsCrypto.majorVersion}.${BuildVersionsCrypto.minorVersion}.${BuildVersionsCrypto.patchVersion}"
-    const val versionNameSdk =
-        "${BuildVersionsSDK.majorVersion}.${BuildVersionsSDK.minorVersion}.${BuildVersionsSDK.patchVersion}"
+    const val publishGroupId = "org.circles.rustcomponents"
+    const val versionNameCrypto = "0.3.15.1"
 }
